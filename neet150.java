6be07ef22b92b62475d94cef690d263d110a292c
wilson-kong <wilson.kong@uqconnect.edu.au>
2024-06-04 15:53:26 +1000

    neetcode twoSum java

--- neet150.java
+++ neet150.java
@@ -3,7 +3,6 @@
  */
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -16,15 +15,15 @@ public class neet150 {
         // int[] nums1 = {1, 2, 3, 4};
         // System.out.println(hasDuplicate(nums1));
 
-        String s = "racecar";
-        String t = "carrace";
-        System.out.println(isAnagram(s, t));
-        s = "jar";
-        t = "jam";
-        System.out.println(isAnagram(s, t));
-        s = "a";
-        t = "ab";
-        System.out.println(isAnagram(s, t));
+        // String s = "racecar";
+        // String t = "carrace";
+        // System.out.println(isAnagram(s, t));
+        // s = "jar";
+        // t = "jam";
+        // System.out.println(isAnagram(s, t));
+        // s = "a";
+        // t = "ab";
+        // System.out.println(isAnagram(s, t));
     }
     
     public static boolean hasDuplicate(int[] nums) {
@@ -50,5 +49,21 @@ public class neet150 {
             }
         }
         return s_letters.size() == 0;
+    }
+
+    public int[] twoSum(int[] nums, int target) {
+        int nums_len = nums.length;
+        int count = 0;
+        for (int i = nums_len - 1; i > 0; i--){
+            for (int j = 0; j < i; j++) {
+                if (nums[nums_len - i - 1] + nums[j + 1 + count] == target) {
+                    int[] output = {nums_len - i - 1, j + 1 + count};
+                    return output;
+                }
+            }
+            count += 1;
+        }
+        int[] output = {};
+        return output;
     }
 }
